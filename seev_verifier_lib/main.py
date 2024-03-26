4c6ec28c73e96c1906430f423ace5120674c16fd
timotheegi <69147986+timotheegi@users.noreply.github.com>
2024-03-26 14:09:32 +0000

    [Spike] [BE] Investigate into the verifier part 2 (#1)
    * Very first version of the verifier's code
    
    * first partial version of the verifier
    
    * Slight edit to mirror the change in the crypto API code that implements
    t = cast(Integer, w.d) - c * _r) % _curve.order rather than t = cast(Integer, w.d) + c * _r) % _curve.order
    
    * removal of a rogue json_stream import
    bump of the version of the library to 0.0.2
    
    * Correction of a None value returned when failing to validate the challenge during the vote proof
    
    * correction of the version of the tag
    
    * - removal of a dev file
    - cleanup of the main file & better display implementation
    - bumping of the libraries version
    
    * Relax the lowest version of python usable
    
    * Addition of the public key validation to guaranty that R and Z have not been tempered with.
    
    * Addition of a type check to enable the specification of the curve being used according to Feng specifications
    
    * Last changes according to Feng feedbacks: mixed up prime and order's values
    
    * Addition of the verification of the audited ballots

---
+++ seev_verifier_lib/main.py
@@ -0,0 +1,86 @@
+import json
+
+import itertools
+from functools import reduce
+from typing import Tuple
+import os
+import sys
+
+from seev_verifier_lib.verifier_lib import verify_signature, load_verify_signature, load_vote_proof, vote_proof, \
+	load_ballot_proof, ballots_proof, load_tally_data, tally_check, load_verify_audited_ballots, verify_audited_ballots
+
+
+def verify(data_raw):
+	"""
+	Verifies the DRE-IP source bulletin board data provided.
+
+	"""
+	print("Verification...")
+	print("----------------------------------------------------------------------------------")
+	data = load_verify_signature(data_raw);	data = zip(*data[:2], itertools.repeat(data[-1]))
+	res_signature: Tuple[bool, ...] = tuple(verify_signature(*d) for d in data)
+	print("\t- SIGNATURE\t", res_signature)
+
+	data = load_vote_proof(data_raw);	data = zip(*data)
+	res_vote_proof: Tuple[bool, ...] = tuple(vote_proof(*d) for d in data)
+	print("\t- VOTE\t\t", res_vote_proof)
+
+	data = load_ballot_proof(data_raw);	data = zip(*data)
+	res_ballot_proof: Tuple[bool, ...] = tuple(ballots_proof(*d) for d in data)
+	print("\t- BALLOT\t", res_ballot_proof)
+
+	data = load_tally_data(data_raw);	data = zip(*data)
+	res_tally_proof: Tuple[bool, ...] = tuple(tally_check(*d) for d in data)
+	print("\t- TALLY\t\t", res_tally_proof)
+	
+	data = load_verify_audited_ballots(data_raw);	data = zip(*data)
+	res_audited_ballots: Tuple[bool, ...] = tuple(verify_audited_ballots(*d) for d in data)
+	print("\t- AUDITED BALLOTS\t\t", res_audited_ballots)
+
+	final_res_signature: bool = reduce(lambda x, y: x and y, res_signature)
+	final_res_vote_proof: bool = reduce(lambda x, y: x and y, res_vote_proof)
+	final_res_ballot_proof: bool = reduce(lambda x, y: x and y, res_ballot_proof)
+	final_res_tally_proof: bool = reduce(lambda x, y: x and y, res_tally_proof)
+	final_res_audited_ballots: bool = reduce(lambda x, y: x and y, res_audited_ballots)
+	election_valid = final_res_signature and final_res_vote_proof and final_res_ballot_proof and final_res_tally_proof and final_res_audited_ballots
+
+	print("----------------------------------------------------------------------------------\n")
+	if election_valid is True:
+		print("The election has been successfully verified.")
+	else:
+		print("The election failed to pass the verification process.")
+
+
+def read_and_parse_file(file_path):
+	"""
+	Reads the content of the provided file and calls the parse function on it.
+
+	:param file_path: Path to the file (can be absolute or relative).
+	"""
+	try:
+		# Normalize the path to ensure compatibility across Unix, Linux, Windows, and MacOS
+		normalized_path = os.path.normpath(file_path)
+
+		# Open and read the file
+		with open(normalized_path, 'r', encoding='utf-8') as fin:
+			data_raw = json.load(fin)
+
+			# Call the parse function on the content of the file
+			verify(data_raw)
+	except FileNotFoundError:
+		print("Error: The file does not exist.")
+	except Exception as e:
+		print(f"An error occurred while attempting to load the file to verify: {e}")
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("Usage: python script.py <path_to_file>")
+		print("<path_to_file> is either absolute or relative to the location of this file")
+	else:
+		# Get the file path from the command line argument
+		file_path = sys.argv[1]
+
+		# Call the function to read and parse the file
+		read_and_parse_file(file_path)
+
