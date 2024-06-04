b60f207ea407305e0f62c4ba919c2354bcbbe6f5
timothee dubuc <timothee@global-initiative.com>
2024-06-04 10:31:45 +0100

    Bump of the version of the verifier from 0.0.7 to 0.0.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name='SEEV-verifier',
-    version='0.0.7',  # Adjust the version as necessary
+    version='0.0.8',  # Adjust the version as necessary
     description='Implementation of the SEEV DRE-IP verifier software',
     author='Timothee Dubuc',
     author_email='timothee@global-initiative.com',
