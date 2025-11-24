a6886d19ddc5007b795627370ead0bb2365c7b26
gi-tomek <tomek@global-initiative.com>
2025-11-24 10:45:06 +0000

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name='SEEV-verifier',
-    version='0.0.14',
+    version='0.0.15',
     description='Implementation of the SEEV DRE-IP verifier software',
     author='Timothee Dubuc',
     author_email='timothee@global-initiative.com',
